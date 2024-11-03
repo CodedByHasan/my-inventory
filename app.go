6e172a8624c50f0c5e1c626fe798f677d6699ead
Hasan <alkhashaab.h@gmail.com>
2024-11-03 18:11:56 +1030

    feat: added /product route to return info about specific product

--- app.go
+++ app.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type App struct {
@@ -32,9 +33,9 @@ func (app *App) Initialise() error {
 	if err != nil {
 		return err
 	}
-
 	app.Router = mux.NewRouter().StrictSlash(true)
 	app.handleRoutes()
+
 	return nil
 }
 
@@ -44,6 +45,7 @@ func (app *App) Run(address string) {
 
 func (app *App) getProducts(w http.ResponseWriter, r *http.Request) {
 	products, err := getProducts(app.DB)
+
 	if err != nil {
 		sendError(w, http.StatusInternalServerError, err.Error())
 		return
@@ -51,8 +53,33 @@ func (app *App) getProducts(w http.ResponseWriter, r *http.Request) {
 	sendResponse(w, http.StatusOK, products)
 }
 
+func (app *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		sendError(w, http.StatusBadRequest, "invalid product ID")
+		return
+	}
+
+	p := product{ID: key}
+	err = p.getProduct(app.DB)
+
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			sendError(w, http.StatusNotFound, "Product not found")
+		default:
+			sendError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	sendResponse(w, http.StatusOK, p)
+}
+
 func (app *App) handleRoutes() {
 	app.Router.HandleFunc("/products", app.getProducts).Methods("GET")
+	app.Router.HandleFunc("/product/{id}", app.getProduct).Methods("GET")
 }
 
 func sendResponse(w http.ResponseWriter, statusCode int, payload interface{}) {
