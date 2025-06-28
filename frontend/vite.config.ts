26f523a12e693316b9190e627b5d59ae4c0983ba
Hasan <88224280+CodedByHasan@users.noreply.github.com>
2025-06-28 15:39:07 +0930

    feat: add react ts frontend that displays all products (#11)
    * feat: add react ts frontend that displays all products and refactored utils so ENVPATH is no longer needed
    
    * fix: removed hard-coded value in vite config
    
    * chore: removing unused image in frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "../");
+  const apiPort = env.VITE_APP_PORT;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': `http://localhost:${apiPort}`
+      },
+    },
+  };
+})
